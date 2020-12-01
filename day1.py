4f9c1dccd9c61a3c071f176c04078a618bf79c8b
Pierre Bousquié <pierre.bousquie@gmail.com>
2020-12-01 22:39:17 +0100

    fucked up refactoring literal style XD

--- day1.py
+++ day1.py
@@ -2,38 +2,50 @@ from itertools import combinations
 from math import prod
 
 
+def read(filename):
+    list_of_expense = []
+    with open(filename) as expense_report:
+        for line in expense_report:
+            list_of_expense.append(int(line))
+    return ExpenseReport(list_of_expense)
+
+
 class ExpenseReport:
-    def __init__(self):
-        self.list_of_expense = []
+    def __init__(self, list_of_expense):
+        self.list_of_expense = list_of_expense
 
-    def get_all_expenses_from_file(self, filename):
-        with open(filename) as expense_report:
-            for line in expense_report:
-                self.list_of_expense.append(int(line))
+    def find_the(self, number):
+        return Entries(combinations(self.list_of_expense, number))
 
-    def find_n_entries_that_sum_to(self, n, target):
-        tuples_of_entries = combinations(self.list_of_expense, n)
 
-        def condition(tuples_of_entries):
-            return sum(tuples_of_entries) == target
+class Entries:
+    def __init__(self, entries):
+        self.entries = entries
 
-        return next(filter(condition, tuples_of_entries))
+    def entries_that_sum_to(self, number):
+        def condition(entries):
+            return sum(entries) == number
+
+        return SelectedEntries(next(filter(condition, self.entries)))
+
+
+class SelectedEntries:
+    def __init__(self, selected_entries):
+        self.selected_entries = selected_entries
+
+    def and_multiply_them_together(self):
+        return prod(self.selected_entries)
 
 
 if __name__ == "__main__":
-    expense_report = ExpenseReport()
-    expense_report.get_all_expenses_from_file("expense_report.txt")
-    target = 2020
-    two_entries = expense_report.find_n_entries_that_sum_to(2, target)
-
+    in_this_list = read("expense_report.txt")
     print(
-        f"""In this list, the two entries that sum to {target} are {two_entries[0]} and {two_entries[1]}.
-Multiplying theme together produces {two_entries[0]} * {two_entries[1]} = {prod(two_entries)}, so the correct 
- answer is {prod(two_entries)}"""
+        in_this_list.find_the(2)
+        .entries_that_sum_to(2020)
+        .and_multiply_them_together()
     )
-
-    three_entries = expense_report.find_n_entries_that_sum_to(3, target)
     print(
-        f"""The product of the three entries that sum to {target} is
-{prod(three_entries)}"""
+        in_this_list.find_the(3)
+        .entries_that_sum_to(2020)
+        .and_multiply_them_together()
     )
