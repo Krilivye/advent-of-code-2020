61ef25142c16ae6aa0ff517223059c4afae08fb6
Pierre Bousquié <pierre.bousquie@gmail.com>
2020-12-01 21:36:14 +0100

    refactor functionnal mixed oo style

--- day1.py
+++ day1.py
@@ -1,45 +1,39 @@
+from itertools import combinations
+from math import prod
+
+
 class ExpenseReport:
     def __init__(self):
         self.list_of_expense = []
 
-    def read_the_expense_report(self):
-        with open("expense_report.txt") as expense_report:
+    def get_all_expenses_from_file(self, filename):
+        with open(filename) as expense_report:
             for line in expense_report:
                 self.list_of_expense.append(int(line))
 
-    def find_the_two_entries_that_sum_to(self, target=2020):
-        for i in self.list_of_expense:
-            if target - i in self.list_of_expense:
-                return i, target - i
+    def find_n_entries_that_sum_to(self, n, target):
+        tuples_of_entries = combinations(self.list_of_expense, n)
 
-    def what_is_the_product_of_the_three_entries_that_sum_to(
-        self, target=2020
-    ):
-        for i in self.list_of_expense:
-            for j in self.list_of_expense:
-                for k in self.list_of_expense:
-                    if i + j + k == target:
-                        return i * j * k
+        def condition(tuples_of_entries):
+            return sum(tuples_of_entries) == target
+
+        return next(filter(condition, tuples_of_entries))
 
 
 if __name__ == "__main__":
     expense_report = ExpenseReport()
-    expense_report.read_the_expense_report()
+    expense_report.get_all_expenses_from_file("expense_report.txt")
     target = 2020
-    number1, number2 = expense_report.find_the_two_entries_that_sum_to(target)
-    multiplyed = number1 * number2
+    two_entries = expense_report.find_n_entries_that_sum_to(2, target)
 
     print(
-        f"""In this list, the two entries that sum to {target} are {number1} and {number2}.
-Multiplying theme together produces {number1} * {number2} = {multiplyed}, so the correct 
-answer is {multiplyed}"""
+        f"""In this list, the two entries that sum to {target} are {two_entries[0]} and {two_entries[1]}.
+Multiplying theme together produces {two_entries[0]} * {two_entries[1]} = {prod(two_entries)}, so the correct 
+ answer is {prod(two_entries)}"""
     )
 
-    three_entries = (
-        expense_report.what_is_the_product_of_the_three_entries_that_sum_to(
-            target
-        )
-    )
+    three_entries = expense_report.find_n_entries_that_sum_to(3, target)
     print(
-        f"""Product of three entries that sum to {target} is {three_entries}"""
+        f"""The product of the three entries that sum to {target} is
+{prod(three_entries)}"""
     )
