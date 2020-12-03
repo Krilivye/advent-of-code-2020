373f805e0020425a77268331e94c194ba1618948
Pierre Bousquié <pierre.bousquie@gmail.com>
2020-12-03 11:50:51 +0100

    refactor a little bit

--- day3/day3.py
+++ day3/day3.py
@@ -1,12 +1,13 @@
+import sys
 from math import prod
 
 
-def get_list_of_lines():
-    with open("input_day3.txt") as pattern:
-        list_of_line = []
+def get_land_pattern(filename):
+    with open(filename) as pattern:
+        lines = []
         for line in pattern:
-            list_of_line.append(line.strip())
-    return list_of_line
+            lines.append(line.strip())
+    return lines
 
 
 def find_next_point(point, slope):
@@ -14,34 +15,47 @@ def find_next_point(point, slope):
     return (point[0] + slope[0], point[1] + slope[1])
 
 
-def is_it_a_tree(point, list_of_line):
+def is_tree(point, list_of_line):
     return list_of_line[point[1]][point[0]] == "#"
 
 
-def check_the_slope(slope, list_of_line):
+def get_trees_on_the_slope(slope, list_of_line):
     point = (0, 0)
     count_trees = 0
     height = len(list_of_line)
     width = len(list_of_line[0])
 
     # print(height, width)
-    for line in list_of_line:
-        point = find_next_point(point, slope)
-        if point[1] >= height:
-            break
-        if point[0] >= width:
-            point = (point[0] % width, point[1])
-        if is_it_a_tree(point, list_of_line):
+    while point[1] < height:
+        if is_tree(point, list_of_line):
             count_trees += 1
+        point = find_next_point(point, slope)
+        point = adjust_width(point, width)
     return count_trees
 
 
-slop_to_test = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+def adjust_width(point, width):
+    if point[0] >= width:
+        point = (point[0] % width, point[1])
+    return point
 
-for slop in slop_to_test:
-    check_the_slope(slop, get_list_of_lines())
 
-
-print(
-    prod([check_the_slope(slop, get_list_of_lines()) for slop in slop_to_test])
-)
+if __name__ == "__main__":
+    for filename in sys.argv[1:]:
+        slop_to_test = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+        trees_in_slopes = [
+            get_trees_on_the_slope(slop, get_land_pattern(filename))
+            for slop in slop_to_test
+        ]
+        print("First Part")
+        print(
+            filename,
+            trees_in_slopes[1],
+            sep=": ",
+        )
+        print("Second Part")
+        print(
+            filename,
+            prod(trees_in_slopes),
+            sep=": ",
+        )
